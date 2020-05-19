81d96def999a032bb79563bb92c1073d55c2216f
Peter Fetterer <kb3gtn@gmail.com>
2020-05-19 12:16:00 -0400

    Added library cache update to install script.

--- Ubuntu2004_install.sh
+++ Ubuntu2004_install.sh
@@ -21,8 +21,9 @@ echo "#                                                                     #"
 echo "#######################################################################"
 echo " This script requires to be run as root since "
 echo " we need to modify system files."
-echo 
-read 'Run this script (Y/n)' $runproc
+echo ""
+read -n1 -p "Run this script (Y/n): " runproc
+echo
 
 # if not Yes, then exit script
 if [[ ! $runproc =~ ^[Yy]$ ]]
@@ -37,27 +38,38 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+
+echo "Installer Starting.."
+
 ###############################
 # Run install process
 ###############################
 
 # Build libbladeRF and install it
 echo "* Making bladeRF build directory."
-mkdir ~/bladeRF_Install
+if [ -d "bladeRF_Install" ]; then
+  echo "Cleaning up old install space.."
+  rm bladeRF_Install -rf
+fi
+mkdir -p bladeRF_Install
+cd bladeRF_Install
+mkdir -p /opt/bladeRF/lib
 echo "* Clone bladeRF repository"
 git clone https://github.com/Nuand/bladeRF
 echo "* Apt install needed dependencies.."
  apt install python3-six python-six python-mako python3-lxml python-lxml python-numpy python3-numpy libusb-1.0-0-dev libusb-1.0-0 build-essential cmake libncurses5-dev libtecla1 libtecla-dev pkg-config git wget
 echo "* Starting Build for BladeRF host software.."
-cd bladeRF
+echo " Change Directory to bladeRF/host..."
+cd bladeRF/host
 mkdir build
 cd build
-cmake ../ -CMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/bladeRF -DINSTALL_UDEV_RULES=ON -DBLADERF_GROUP=bladerf
+cmake ../ -DCMAKE_INSTALL_PREFIX=/opt/bladeRF
 make -j4
 echo "* Creating bladeRF install directories and perform installization of bladeRF software"
 mkdir -p /opt/bladeRF/etc
 make install
-
+echo "* libbladeRF build complete...."
+echo "------------------------------------------------"
 
 # create bladerf_paths.sh for profile.d .. 
 echo "* Create bladeRF_path.sh in /etc/profile.d/bladeRF_path.sh"
@@ -72,6 +84,8 @@ echo "* create library path in /etc/ld.so.conf.d/bladeRF.conf"
 cat > /etc/ld.so.conf.d/bladeRF.conf <<EOF
 /opt/bladeRF/lib
 EOF
+# update library cache
+ldconfig
 
 # Add udev rules
 echo "* creating udev rules for bladeRF in /etc/udev/rules.d/88-nuand-bladerf.rules"
@@ -82,17 +96,24 @@ ATTR{idVendor}=="2cf0", ATTR{idProduct}=="5250", MODE="660", GROUP="bladerf"
 ATTR{idVendor}=="1d50", ATTR{idProduct}=="6066", MODE="660", GROUP="bladerf"
 EOF
 # reload udev rules
-udevadmin control --reload-rules
+udevadm control --reload-rules
 
 # grab fpga images from nuand
+echo "------------------------------------------------------"
 echo "* Grabbing FPGA and firmware from NUAND..."
 cd /opt/bladeRF/etc
-wget "https://www.nuand.com/fpga/hostedxA4-latest.rbf"
+echo -n "Downloading hostedxA4 image.."
+wget "https://www.nuand.com/fpga/hostedxA4-latest.rbf" -q
 mv hostedxA4-latest.rbf hostedxA4.rbf
-wget "https://www.nuand.com/fpga/hostedxA9-latest.rbf"
-mv hostedxA4-latest.rbf hostedxA9.rbf
-wget "https://www.nuand.com/fx3/bladeRF_fw_latest.img"
-mv bladeRF_fw_lastest.img bladeRF.img
+echo "  OK"
+echo -n "DOwnloading hostedxA9 image.."
+wget "https://www.nuand.com/fpga/hostedxA9-latest.rbf" -q
+mv hostedxA9-latest.rbf hostedxA9.rbf
+echo "  OK"
+echo -n "Downloading fx3 firmware.."
+wget "https://www.nuand.com/fx3/bladeRF_fw_latest.img" -q
+mv bladeRF_fw_latest.img bladeRF.img
+echo "  OK"
 
 # create bladerf group
 groupadd bladerf
